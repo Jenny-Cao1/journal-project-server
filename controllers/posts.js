996e4fb87d323087d5a24808fdda5b59fed60391
jncao49 <jncao49@gmail.com>
2022-11-23 12:33:21 -0500

    fix: point to correct ID for updated post

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,10 +29,10 @@ export const createPost = async (req, res) => {
 export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body;
-    console.log('here', req, res);
+    
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
-    console.log('updated post: ', updatedPost);
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
     res.json(updatedPost);
 }
