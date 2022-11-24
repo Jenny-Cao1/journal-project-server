b84560e3cd8f233febcb6537965f92cb94b5ef48
jncao49 <jncao49@gmail.com>
2022-11-24 12:53:27 -0500

    chore: added .env template

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
 const app = express();
 
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,10 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://jennycao:jennycao-dev@cluster0.rcsnjrj.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
-
-//Connect to our MongoDB
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+//Connect to MongoDB
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(process.env.PORT, ()=> console.log(`Server running on port: ${process.env.PORT}`)))
     .catch((error) => console.log(error.message));
